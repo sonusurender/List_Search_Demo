6779691c6c9c95f78709208e17935f4a1376da7b
Surender Kumar <sonu.surendra0@gmail.com>
2019-02-08 14:15:32 +0530

    initial commit

---
+++ app/src/main/java/com/listsearch_demo/helper/FilterType.java
@@ -0,0 +1,11 @@
+package com.listsearch_demo.helper;
+
+/**
+ * Created by sonu on 14/02/17.
+ */
+
+/*  Enum Class to identify the type of Filter/Search type  */
+
+public enum FilterType {
+    NAME, NUMBER, EMAIL;
+}
